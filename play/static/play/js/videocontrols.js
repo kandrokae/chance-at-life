226f337b207a207a953a3ffe2f77c24856f670ad
Karel Androkae <kandrokae@gmail.com>
2020-04-20 09:29:08 -0400

    ajax data unusable fix

--- play/static/play/js/videocontrols.js
+++ play/static/play/js/videocontrols.js
@@ -127,7 +127,169 @@ window.onload = function() {
 		dataType: 'json',
 		success: function(data) {
 			questionData = data;
-			promptChoices(questionData);
+			// Update the seek bar as the video plays
+			video.addEventListener("timeupdate", function(questionData) {
+				// Calculate the slider value
+				var value = (100 / video.duration) * video.currentTime;
+
+				// Taxes handling at 06:15
+				if (video.currentTime >= 374 && video.currentTime <= 374.3) {
+					var oldpercent = progressBar.style.width.slice(0, -1);
+					var oldPrice = progressText.innerHTML.substring(1);
+					progressBar.style.width = parseInt(oldpercent) - (500/55) + "%";
+					var currentPrice = parseInt(oldPrice) - 5;
+					progressText.innerHTML = "$" + currentPrice;
+					console.log("works");
+				};
+
+				//Start button shows up when it has to
+				if (video.currentTime >= 26.7 && video.currentTime <= 37.4) {
+					startButton.style.visibility = "visible";
+				} else {
+					startButton.style.visibility = "hidden";
+				};
+
+				if (video.currentTime >= 42.5) {
+					progressContainer.style.opacity = "1";
+				} else {
+					progressContainer.style.opacity = "0";
+				};
+				
+				var oldpercent = progressBar.style.width.slice(0, -1);
+				if (parseInt(oldpercent) <= 30) {
+					progressBar.style["background-color"] = "#f54d28";
+				} else if (parseInt(oldpercent) <= 50 && parseInt(oldpercent) >= 30.5) {
+					progressBar.style["background-color"] = "#ffa84e";
+				} else {
+					progressBar.style["background-color"] = "#3399CC";
+				};
+				
+
+				if (video.currentTime >= 43) {
+					flag.style.visibility = "visible";
+				} else {
+					flag.style.visibility = "hidden";
+				};
+
+				var oldPrice = progressText.innerHTML.substring(1);
+
+				if (oldPrice <= 0) {
+					if (video.currentTime <= loseTime - 0.2) {
+						video.pause();
+						progressText.innerHTML = "";
+						progressBar.style.width = "0%";
+						video.currentTime = loseTime;
+						document.body.style.backgroundColor = "#f54d28";
+						video.play();
+					};
+
+				} else if (oldPrice > 0) {
+					if (video.currentTime > loseTime - 0.7 && video.currentTime < loseTime - 0.3) {
+						video.pause();
+						video.currentTime = winTime;
+						document.body.style.backgroundColor = "#3399CC";
+						video.play();
+					};
+				};
+
+				if (video.currentTime >= video.duration - 1) {
+					window.location.href = 'http://chance-at-life.com/';
+				};
+
+				if (video.currentTime >= loseTime - 0.5) {
+					window.location.href = 'http://chance-at-life.com/';
+				};
+
+
+				for (i=0; i < questionData.length; i++) {
+
+					if (video.currentTime >= questionData[i].timestamp + 2 && video.currentTime <= questionData[i].choice1_timestamp - 0.2) {
+						prompt.style.visibility = "visible";
+						prompt.style.opacity = "1";
+						timerContainer.style.visibility = "visible";
+						timerContainer.style.opacity = "1";
+						var timer = questionData[i].choice1_timestamp - questionData[i].timestamp - 2;
+						timerBar.style.transition = "all " + timer + "s ease-in-out";
+						timerBar.style.width = "100%";
+						questionText.innerHTML = questionData[i].question_text;
+						choice1.innerHTML = questionData[i].choice1_text;
+						choice2.innerHTML = questionData[i].choice2_text;
+						price1.innerHTML = "$" + questionData[i].price1;
+						price2.innerHTML = "$" + questionData[i].price2;
+
+						var choice1time = questionData[i].choice1_timestamp;
+						var choice2time = questionData[i].choice2_timestamp;
+						var choice1price = questionData[i].price1;
+						var choice2price = questionData[i].price2;
+						var oldpercent = progressBar.style.width.slice(0, -1);
+						var oldPrice = progressText.innerHTML.substring(1);
+
+
+						//Event listener for the button1 button
+						button1.addEventListener("click", function(){
+							if (oldpercent > parseInt(choice1price)*100/55) {
+								var currentPrice;
+								video.currentTime = choice1time;
+								progressBar.style.width = parseInt(oldpercent) - (parseInt(choice1price)*100/55) + "%";
+								currentPrice = parseInt(oldPrice) - parseInt(choice1price);
+								progressText.innerHTML = "$" + currentPrice;
+							} else {
+								progressBar.style.width = '0%';
+								progressText.innerHTML = "$0";
+								myProgress.style["background-color"] = "#f54d28";
+								video.currentTime = loseTime;
+
+							};
+						});
+
+						//Event listener for the button2 button
+						button2.addEventListener("click", function(){
+							if (oldpercent > parseInt(choice2price)*100/55) {
+								var currentPrice;
+								video.currentTime = choice2time;
+								progressBar.style.width = parseInt(oldpercent) - (parseInt(choice2price)*100/55) + "%";
+								currentPrice = parseInt(oldPrice) - parseInt(choice2price);
+								progressText.innerHTML = "$" + currentPrice;
+							} else {
+								progressBar.style.width = '0%';
+								progressText.innerHTML = "$0";
+								myProgress.style["background-color"] = "#f54d28";
+								video.currentTime = loseTime;
+								
+							};
+						});
+
+						if (video.currentTime >= questionData[i].choice1_timestamp - 0.5 && video.currentTime <= questionData[i].choice1_timestamp - 0.2 ) {
+							var choice1price = questionData[i].price1;
+							var oldpercent = progressBar.style.width.slice(0, -1);
+							var oldPrice = progressText.innerHTML.substring(1);
+							progressBar.style.width = parseInt(oldpercent) - (parseInt(choice1price)*100/55) + "%";
+							currentPrice = parseInt(oldPrice) - parseInt(choice1price);
+							progressText.innerHTML = "$" + currentPrice;
+
+						};
+
+						
+					} else if (video.currentTime >= questionData[i].choice1_timestamp && video.currentTime < questionData[i].skip_to) {
+						prompt.style.opacity = "0";
+						prompt.style.visibility = "hidden";
+						timerContainer.style.opacity = "0";
+						timerContainer.style.visibility = "hidden";
+						timerBar.style.transition = "all " + 0.1 + "s ease-in-out";
+						timerBar.style.width = "0%";
+
+
+						if (video.currentTime >= questionData[i].choice2_timestamp - 0.5 && video.currentTime <= questionData[i].choice2_timestamp - 0.1 ) {
+							video.currentTime = questionData[i].skip_to;
+
+						};
+
+					};
+				};
+
+				// Update the slider value
+				seekBar.value = value;
+			});
 			console.log(questionData);
 		},
 		error: function(error_data) { 
@@ -141,183 +303,6 @@ window.onload = function() {
 	var progressBar = document.getElementById("myBar");
 	progressBar.style.width = '100%';
 	var oldpercent = progressBar.style.width.slice(0, -1);
-
-	function promptChoices(dataParameter) {
-		// Update the seek bar as the video plays
-		video.addEventListener("timeupdate", function(questionData) {
-			// Calculate the slider value
-			var value = (100 / video.duration) * video.currentTime;
-
-			// Taxes handling at 06:15
-			if (video.currentTime >= 374 && video.currentTime <= 374.3) {
-				var oldpercent = progressBar.style.width.slice(0, -1);
-				var oldPrice = progressText.innerHTML.substring(1);
-				progressBar.style.width = parseInt(oldpercent) - (500/55) + "%";
-				var currentPrice = parseInt(oldPrice) - 5;
-				progressText.innerHTML = "$" + currentPrice;
-				console.log("works");
-			};
-
-			//Start button shows up when it has to
-			if (video.currentTime >= 26.7 && video.currentTime <= 37.4) {
-				startButton.style.visibility = "visible";
-			} else {
-				startButton.style.visibility = "hidden";
-			};
-
-			if (video.currentTime >= 42.5) {
-				progressContainer.style.opacity = "1";
-			} else {
-				progressContainer.style.opacity = "0";
-			};
-			
-			var oldpercent = progressBar.style.width.slice(0, -1);
-			if (parseInt(oldpercent) <= 30) {
-				progressBar.style["background-color"] = "#f54d28";
-			} else if (parseInt(oldpercent) <= 50 && parseInt(oldpercent) >= 30.5) {
-				progressBar.style["background-color"] = "#ffa84e";
-			} else {
-				progressBar.style["background-color"] = "#3399CC";
-			};
-			
-
-			if (video.currentTime >= 43) {
-				flag.style.visibility = "visible";
-			} else {
-				flag.style.visibility = "hidden";
-			};
-
-			var oldPrice = progressText.innerHTML.substring(1);
-
-			/*if (video.currentTime <= loseTime - 0.2 && video.currentTime >= loseTime - 0.5 && parseInt(oldPrice) > 0) {
-				video.currentTime = winTime;
-			};
-			if (parseInt(oldPrice) <= 0) {
-				progressText.style.opacity = 0;
-				progressText.innerHTML = "$1";
-				video.pause();
-				video.currentTime = loseTime;
-				video.play();
-			}; */
-
-			if (oldPrice <= 0) {
-				if (video.currentTime <= loseTime - 0.2) {
-					video.pause();
-					progressText.innerHTML = "";
-					progressBar.style.width = "0%";
-					video.currentTime = loseTime;
-					document.body.style.backgroundColor = "#f54d28";
-					video.play();
-				};
-
-			} else if (oldPrice > 0) {
-				if (video.currentTime > loseTime - 0.7 && video.currentTime < loseTime - 0.3) {
-					video.pause();
-					video.currentTime = winTime;
-					document.body.style.backgroundColor = "#3399CC";
-					video.play();
-				};
-			};
-
-			if (video.currentTime >= video.duration - 1) {
-				window.location.href = 'http://chance-at-life.com/';
-			};
-
-			if (video.currentTime >= loseTime - 0.5) {
-				window.location.href = 'http://chance-at-life.com/';
-			};
-
-
-			for (i=0; i < questionData.length; i++) {
-
-				if (video.currentTime >= questionData[i].timestamp + 2 && video.currentTime <= questionData[i].choice1_timestamp - 0.2) {
-					prompt.style.visibility = "visible";
-					prompt.style.opacity = "1";
-					timerContainer.style.visibility = "visible";
-					timerContainer.style.opacity = "1";
-					var timer = questionData[i].choice1_timestamp - questionData[i].timestamp - 2;
-					timerBar.style.transition = "all " + timer + "s ease-in-out";
-					timerBar.style.width = "100%";
-					questionText.innerHTML = questionData[i].question_text;
-					choice1.innerHTML = questionData[i].choice1_text;
-					choice2.innerHTML = questionData[i].choice2_text;
-					price1.innerHTML = "$" + questionData[i].price1;
-					price2.innerHTML = "$" + questionData[i].price2;
-
-					var choice1time = questionData[i].choice1_timestamp;
-					var choice2time = questionData[i].choice2_timestamp;
-					var choice1price = questionData[i].price1;
-					var choice2price = questionData[i].price2;
-					var oldpercent = progressBar.style.width.slice(0, -1);
-					var oldPrice = progressText.innerHTML.substring(1);
-
-
-					//Event listener for the button1 button
-					button1.addEventListener("click", function(){
-						if (oldpercent > parseInt(choice1price)*100/55) {
-							var currentPrice;
-							video.currentTime = choice1time;
-							progressBar.style.width = parseInt(oldpercent) - (parseInt(choice1price)*100/55) + "%";
-							currentPrice = parseInt(oldPrice) - parseInt(choice1price);
-							progressText.innerHTML = "$" + currentPrice;
-						} else {
-							progressBar.style.width = '0%';
-							progressText.innerHTML = "$0";
-							myProgress.style["background-color"] = "#f54d28";
-							video.currentTime = loseTime;
-
-						};
-					});
-
-					//Event listener for the button2 button
-					button2.addEventListener("click", function(){
-						if (oldpercent > parseInt(choice2price)*100/55) {
-							var currentPrice;
-							video.currentTime = choice2time;
-							progressBar.style.width = parseInt(oldpercent) - (parseInt(choice2price)*100/55) + "%";
-							currentPrice = parseInt(oldPrice) - parseInt(choice2price);
-							progressText.innerHTML = "$" + currentPrice;
-						} else {
-							progressBar.style.width = '0%';
-							progressText.innerHTML = "$0";
-							myProgress.style["background-color"] = "#f54d28";
-							video.currentTime = loseTime;
-							
-						};
-					});
-
-					if (video.currentTime >= questionData[i].choice1_timestamp - 0.5 && video.currentTime <= questionData[i].choice1_timestamp - 0.2 ) {
-						var choice1price = questionData[i].price1;
-						var oldpercent = progressBar.style.width.slice(0, -1);
-						var oldPrice = progressText.innerHTML.substring(1);
-						progressBar.style.width = parseInt(oldpercent) - (parseInt(choice1price)*100/55) + "%";
-						currentPrice = parseInt(oldPrice) - parseInt(choice1price);
-						progressText.innerHTML = "$" + currentPrice;
-
-					};
-
-					
-				} else if (video.currentTime >= questionData[i].choice1_timestamp && video.currentTime < questionData[i].skip_to) {
-					prompt.style.opacity = "0";
-					prompt.style.visibility = "hidden";
-					timerContainer.style.opacity = "0";
-					timerContainer.style.visibility = "hidden";
-					timerBar.style.transition = "all " + 0.1 + "s ease-in-out";
-					timerBar.style.width = "0%";
-
-
-					if (video.currentTime >= questionData[i].choice2_timestamp - 0.5 && video.currentTime <= questionData[i].choice2_timestamp - 0.1 ) {
-						video.currentTime = questionData[i].skip_to;
-
-					};
-
-				};
-			};
-
-			// Update the slider value
-			seekBar.value = value;
-		});
-	};
 
 
 	// Pause the video when the seek handle is being dragged
